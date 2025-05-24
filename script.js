9df27bd72b280ccdaa76440bbc448e29fabad853
07-mvieira <caio.vieira2807@gmail.com>
2025-05-24 17:43:55 -0300

    a

--- script.js
+++ script.js
@@ -3,3 +3,31 @@
 // personagem principal pode se mover em 8 direções e atacar em 4
 // inimigos atiram projéteis para matar o jogador
 // contador de fps para animações
+let keysPressed = {}
+window.onkeyup   = function(e) {keysPressed[e.key] = false;}
+window.onkeydown = function(e) {keysPressed[e.key] = true;}
+
+const Player = {
+    obj: document.getElementById("player"),
+    vis: document.getElementById("player-graphics"),
+}
+
+const timer = document.getElementById("timer")
+
+const startButton = document.getElementById("start")
+
+function gameStart() {
+    startButton.style.display = `none`;
+    timer.style.display = `block`;
+
+    Player.obj.style.display = `block`;
+    Player.obj.style.top = `50%`; Player.obj.style.left = `50%`; Player.obj.style.transform = `translate(-50%, -50%)`
+    gameLoop();
+}
+
+function gameLoop() {
+    /*
+        if (keysPressed[`ArrowUp`]) {
+            Player.obj.style.top = `calc($let Player.x}-1%)`
+    }*/
+}
